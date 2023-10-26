6354a4479145939cafa2fec5b304d2f666ad87e2
Mohammed Al-Tabaei <117546290+42778312@users.noreply.github.com>
2023-10-26 09:06:04 +0200

    Add project files.

---
+++ loginForm/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace loginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_User = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Pass = new System.Windows.Forms.TextBox();
+            this.txt_User = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_User
+            // 
+            this.lbl_User.AutoSize = true;
+            this.lbl_User.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_User.ForeColor = System.Drawing.Color.White;
+            this.lbl_User.Location = new System.Drawing.Point(32, 129);
+            this.lbl_User.Name = "lbl_User";
+            this.lbl_User.Size = new System.Drawing.Size(255, 55);
+            this.lbl_User.TabIndex = 0;
+            this.lbl_User.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(43, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // txt_Pass
+            // 
+            this.txt_Pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Pass.Location = new System.Drawing.Point(359, 215);
+            this.txt_Pass.Multiline = true;
+            this.txt_Pass.Name = "txt_Pass";
+            this.txt_Pass.Size = new System.Drawing.Size(394, 82);
+            this.txt_Pass.TabIndex = 2;
+            // 
+            // txt_User
+            // 
+            this.txt_User.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_User.Location = new System.Drawing.Point(359, 129);
+            this.txt_User.Multiline = true;
+            this.txt_User.Name = "txt_User";
+            this.txt_User.Size = new System.Drawing.Size(394, 78);
+            this.txt_User.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(395, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 82);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_User);
+            this.Controls.Add(this.txt_Pass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_User);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_User;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Pass;
+        private System.Windows.Forms.TextBox txt_User;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ loginForm/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user, pass;
+
+            user = txt_User.Text;
+            pass = txt_Pass.Text;   
+
+            if (user == "root"&& pass == "root")
+            {
+                MessageBox.Show("Login Successful");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+    }
+}
